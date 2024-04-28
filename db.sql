681f1ff761f4c147f2ac36cd436d221ba001ae05
pyropecs <praveengopu0105@gmail.com>
2024-04-28 12:45:51 +0530

    final deploy

--- db.sql
+++ db.sql
@@ -12,3 +12,23 @@ created_at timestamp not null,
 updated_at timestamp not null);
 
 -- X58YB5THD2VM2NAHWG938BFF
+
+create table owners(
+owner_id serial PRIMARY KEY not null,
+vehicle_type varchar(20) not null,
+bike_name varchar(50) not null,
+plate_number varchar(20) not null,
+owner_photo text not null,
+phone_no varchar(20) not null,
+owner_name varchar(200) not null,
+created_at timestamp not null,
+updated_at timestamp not null);
+
+create table challans(
+challan_id serial primary key not null,
+fine_amount DECIMAL(10,2) NOT NULL,
+challan_created_at timestamp not null,
+plate_number varchar(20) not null,
+violations text[] not null,
+fine_amount int not null,
+);
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { Pool } = require('pg');
 const dotenv = require("dotenv")
+dotenv.config()
 const {v2:cloudinary} = require("cloudinary")
 const bodyParser = require("body-parser")
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const twilio = require('twilio')(accountSid, authToken);
 
-dotenv.config()
+
 const app = express();
 const port =  process.env.PORT || 3000;
 
@@ -58,15 +59,15 @@ const {data} = await dummydatares.json()
   try {
     const created_at = Date.now();
     const updated_at = Date.now()
-    const { vehicle_type, bike_name,plate_number,violations,fine_amount,phone_no,owner_name /* ...other fields */ } = await req.body[0];
+    const { vehicle_type, bike_name,phone_no,owner_name,plate_number /* ...other fields */ } = await req.body[0];
 const owner_photo = await data[0].picture
 console.log("owner_photo",owner_photo)
 
-const values = [vehicle_type, bike_name,plate_number,violations,fine_amount,owner_photo,phone_no,owner_name,created_at,updated_at];
+const values = [vehicle_type, bike_name,plate_number,owner_photo,phone_no,owner_name,created_at,updated_at];
     // const queryDEP = `INSERT INTO owners (owner_id,vehicle_type, bike_name
     //         ,plate_number,violations,fine_amount,owner_photo,created_at,updated_at)
     //                VALUES (DEFAULT,$1,$2,$3,$4,$5,$6,$7,NOW(),NOW())`;
-const query = `INSERT INTO owners(vehicle_type,bike_name,plate_number,violations,fine_amount,owner_photo,phone_no,owner_name,created_at,updated_at) VALUES($1,$2,$3,$4,$5,$6,$7,$8,to_timestamp($9/1000.0),to_timestamp($10/1000.0))`
+const query = `INSERT INTO owners(vehicle_type,bike_name,plate_number,owner_photo,phone_no,owner_name,created_at,updated_at) VALUES($1,$2,$3,$4,$5,$6,to_timestamp($7/1000.0),to_timestamp($8/1000.0))`
     
     console.log(await values)
     const response = await pool.query(query, values);
@@ -83,17 +84,14 @@ const query = `INSERT INTO owners(vehicle_type,bike_name,plate_number,violations
 // Replace with your PostgreSQL credentials
 
 
-app.get('/get', async (req, res) => {
-  try {
-    const query = `SELECT * FROM your_table `; // Adjust the query for specific columns
-    const response = await pool.query(query);
+app.post('/get', async (req, res) => {
+const body = await req.body[0]
+const {challan_id} = await body;
+const getQuery = await pool.query(`select * from owners inner join challans on owners.plate_number = challans.plate_number where challan_id = $1;`,[challan_id])
+const rows = await getQuery.rows
+console.log(getQuery)
+res.json({result:rows})
 
-    const rows = response.rows; // Array of objects representing table rows
-    res.json(rows); // Send the retrieved data as JSON
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error retrieving data' });
-  }
 });
 
  //[{
@@ -107,32 +105,62 @@ app.get('/get', async (req, res) => {
 
 // }]
 app.post("/violation",async(req,res)=>{
-const body = await req.body; // license_plate ,violations 
+const body = await req.body[0]; // license_plate ,violations 
 console.log("violation_body",body)
 const {plate_number,violations,fine_amount} = await body
 
     
+// try{
+//   const now = Date.now()
+//   // const amount = await pool.query(`select fine_amount from owners where plate_number=$1`,)
+//   const query = `update owners set violations=$1,updated_at=to_timestamp($2/1000.0),fine_amount = fine_amount + $3 where plate_number = $4`
+// const values = [violations,now,fine_amount,plate_number]
+// const response = await pool.query(query,values)
+// console.log("violation_response",response)
+// res.json({message:"violated updated successfully"})
+// const [data] = req.body
+// twilio.messages
+//     .create({
+//         body: `hello praveen, you violated ${data.violations} in vehicle having ${data.plate_number} we have charged you rupees ${data.fine_amount}`,
+//         from: 'whatsapp:+14155238886',
+//         to: 'whatsapp:+917338726600'
+//     })
+//     .then(message => console.log(message.sid))
+
+// }catch(error){
+//   console.error(error)
+//   res.status(500).json({ message: 'Error on updating violations data' });
+// }
+
 try{
-  const now = Date.now()
-  // const amount = await pool.query(`select fine_amount from owners where plate_number=$1`,)
-  const query = `update owners set violations=$1,updated_at=to_timestamp($2/1000.0),fine_amount = fine_amount + $3 where plate_number = $4`
-const values = [violations,now,fine_amount,plate_number]
-const response = await pool.query(query,values)
-console.log("violation_response",response)
-res.json({message:"violated updated successfully"})
-const [data] = req.body
-twilio.messages
-    .create({
-        body: `hello praveen, you violated ${data.violations} in vehicle having ${data.plate_number} we have charged you rupees ${data.fine_amount}`,
+
+const now = Date.now()
+const values = [Number(fine_amount),now,plate_number,violations];
+
+const query = await pool.query(`insert into  challans (fine_amount,challan_created_at,plate_number,violations) values($1,to_timestamp($2/1000.0),$3,$4);`,values)
+console.log("violation_response",query)
+const getQuery = await pool.query('select challan_id from challans where plate_number = $1',[body.plate_number])
+const {challan_id} = getQuery.rows[0]
+res.json({message:"violation added successfully"})
+try{
+
+
+    twilio.messages.create({
+        body: `hello praveen, you violated ${body.violations} in vehicle having ${body.plate_number} \n we have charged you rupees ${body.fine_amount} \n here is an challan id https://relaxed-salamander-d2266d.netlify.app/${challan_id} go to this website link to pay it `,
         from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+917338726600'
+        to: 'whatsapp:+919345699817'
     })
     .then(message => console.log(message.sid))
+  }catch(err){
+    console.log("violation added successfully but not message sent")
+    
+  }
+}catch(err){
+  console.log("violation error".err)
+  res.json({message:"error while retriveing data"})
 
-}catch(error){
-  console.error(error)
-  res.status(500).json({ message: 'Error on updating violations data' });
 }
+
 
 
 })
